a7cc4ba31ff1244911a6cd4af1cc6d8d62d6e35a
Bibibool <dorinvilbruel@gmail.com>
2025-04-12 15:58:57 +0200

    Updated airflow config for db and redis

--- log-analysis/dags/fill_table.py
+++ log-analysis/dags/fill_table.py
@@ -1,44 +1,46 @@
 from airflow.decorators import dag, task
-from datetime import datetime 
+from datetime import datetime
 import psycopg2
 import os
 
 @dag(
     default_args={
         "owner": "sem",
-        "start_date": datetime(2024,4,5)
+        "start_date": datetime(2024, 4, 5)
     },
-        schedule_interval="@once",
-        catchup=False, 
-        tags=["archives-log-analysis"])
-
-def fill_table():
+    schedule_interval="@once",
+    catchup=False,
+    tags=["archives-log-analysis"]
+)
+def fill_table_v1():
     """Data from the aws raw bucket to populate the analytics postgres table"""
     # Define the tasks
-    @task 
-    def create_connection():
-        """Create a connection to the database"""    
+    @task
+    def check_database_connection_v1():
+        """Check the connection to the database"""
         try:
             conn = psycopg2.connect(
-                dbname=os.getenv("DB_NAME"),
-                user=os.getenv("DB_USER"),
-                password=os.getenv("DB_PASSWORD"),
-                host=os.getenv("DB_HOST")
+                dbname=os.getenv("AIRFLOW_DATABASE_DBNAME"),
+                user=os.getenv("AIRFLOW_DATABASE_USER"),
+                password=os.getenv("AIRFLOW_DATABASE_PASSWORD"),
+                host=os.getenv("AIRFLOW_DATABASE_HOST")
             )
             print("Connection to database successful")
 
             cursor = conn.cursor()
             cursor.execute("""
-                SELECT datname 
-                FROM pg_database 
+                SELECT datname
+                FROM pg_database
                 WHERE datistemplate = false; """)
             databases = cursor.fetchall()
-            print(databases)
+            print("Databases:", databases)
+            conn.close()
             return databases
         except Exception as e:
-            print(e)
-            print("Connection to database failed. Verify your credentials in the .env file")
+            print(f"Connection to database failed: {e}")
+            print("Verify your credentials in the .env file and Docker Compose configuration.")
+            raise  # It's good practice to re-raise the exception for Airflow to track failure
 
-    create_connection_task = create_connection()
+    check_db_task = check_database_connection_v1()
 
-fill_table()
+fill_table_v1()
